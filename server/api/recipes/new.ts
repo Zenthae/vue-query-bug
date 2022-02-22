a723af855076abce695388c604610a1afa043ece
Zenthae <Zenthae@gmail.com>
2022-02-22 16:58:48 +0100

    api

--- server/api/recipes/new.ts
+++ server/api/recipes/new.ts
@@ -1,6 +1,11 @@
 import { Handle } from 'h3';
 
 const endpoint: Handle = (_req, _res) => {
+  if (_req.method !== 'POST') {
+    _res.statusCode = 405;
+    return _res.end();
+  }
+
   _res.statusCode = 200;
   return {};
 };
